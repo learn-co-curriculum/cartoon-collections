55e7c2c9603641c3c2cd9972f2c533fb74cef3fc
Spencer Rogers <spencer1248@gmail.com>
2014-01-30 13:46:01 -0500

    Add exercise for map

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "Cartoon Collections" do
-  describe "#number_the_dwarves" do
+  describe "number_the_dwarves" do
     it "prints out the 7 dwarfs in a numbered list" do
       dwarves = ["Dopey", "Grumpy", "Bashful"]
 
@@ -14,6 +14,17 @@ describe "Cartoon Collections" do
       expect(output).to match /1.*Dopey/
       expect(output).to match /2.*Grumpy/
       expect(output).to match /3.*Bashful/
+    end
+  end
+
+  describe "summon_captain_planet" do
+    it "makes things more exciting" do
+      words = %w(apple banana orange)
+
+      result = summon_captain_planet(words)
+      expect(result).to match /Apple!/ 
+      expect(result).to match /Banana!/ 
+      expect(result).to match /Orange!/ 
     end
   end
 end
