c23218773fb7ab856b72843be107db53dbc47cee
Spencer Rogers <spencer1248@gmail.com>
2014-01-30 13:32:06 -0500

    Add helpful comment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,5 +8,13 @@ dwarves = [
   "Happy"
 ]
 
+# Print out the following list:
+# 1. Doc
+# 2. Dopey
+# 3. Bashful
+# etc. for all the dwarves
+#
+# Use the each_with_index iterator:
+# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-each_with_index
 def number_the_dwarves(dwarves)
 end
