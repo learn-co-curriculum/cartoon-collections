83f52bf8cb2aad71e23b849dfb2a49acf4f181d6
Spencer Rogers <spencer1248@gmail.com>
2014-02-04 10:40:26 -0500

    Add solutions

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,9 @@ dwarves = [
 # Use the "each_with_index" method:
 # http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-each_with_index
 def roll_call_dwarves(dwarves)
-  # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  end
 end
 
 roll_call_dwarves(dwarves)
@@ -39,7 +41,7 @@ planeteer_calls = %w(earth wind fire water heart)
 
 # Expected return value: ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
 summon_captain_planet(planeteer_calls)
@@ -51,7 +53,7 @@ summon_captain_planet(planeteer_calls)
 # http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-any-3F
 
 def long_planeteer_calls(planeteer_calls)
-  # Your code here
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
 long_planeteer_calls(planeteer_calls)
@@ -79,6 +81,11 @@ long_planeteer_calls(planeteer_calls)
 potentially_cheesy_items = ["umbrella", "spinach", "cheddar", "helicopter"]
 
 def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
 
 find_the_cheese(potentially_cheesy_items)
