4ef442d4d6f66be044a54593fb31b5153a15edaa
Spencer Rogers <spencer1248@gmail.com>
2014-02-04 10:40:37 -0500

    Fix tests

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -32,9 +32,9 @@ describe "Cartoon Collections" do
     it "tells us the longest planeteer call" do
       long_planeteer_calls = %w(earth wind fire water heart)
 
-      expect(long_planeteer_calls(planeteer_calls)).to eq(true)
+      expect(long_planeteer_calls(long_planeteer_calls)).to eq(true)
 
-      short_planeteer_calls = %w(wind fire water heart)
+      short_planeteer_calls = %w(wind fire)
 
       expect(long_planeteer_calls(short_planeteer_calls)).to eq(false)
     end
