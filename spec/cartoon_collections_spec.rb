4a6556e0a4041f7b1977e4fda9981c273e347bff
Danny-Sullivan <dsully360@gmail.com>
2015-10-26 16:42:48 -0400

    updated specs

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -19,46 +19,47 @@ describe "Cartoon Collections" do
 
   describe "#summon_captain_planet" do
     it "returns an array with the same number of elements that it was given" do
-      veggies = %w(carrot cucumber pepper)
+      veggies = ["carrot", "cucumber", "pepper"]
       result = summon_captain_planet(veggies)
       expect(result.class).to eq(Array)
       expect(result.length).to eq(3)
     end
 
     it "capitalizes each element and adds an exclamation mark" do
-      fruits = %w(apple banana orange)
+      fruits = ["apple", "banana", "orange"]
       result = summon_captain_planet(fruits)
       ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
+    end
 
-      veggies = %w(carrot cucumber pepper)
+    it "capitalizes each element and adds an exclamation mark" do
+      veggies = ["carrot", "cucumber", "pepper"]
       result = summon_captain_planet(veggies)
       ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
     end
   end
 
   describe "#long_planteer_calls" do
-    it "tells us if any calls are longer than 4 characters" do
-      long_planteer_calls = %w(earth wind fire water heart)
+    it "returns true if any calls are longer than 4 characters" do
+      long_planteer_calls = ["earth", "wind", "fire", "water", "heart"]
       expect(long_planteer_calls(long_planteer_calls)).to eq(true)
+    end
 
-      short_planeteer_calls = %w(wind fire)
+    it "returns false if all calls are shorter than 4 characters" do
+      short_planeteer_calls = ["wind", "fire"]
       expect(long_planteer_calls(short_planeteer_calls)).to eq(false)
     end
+
   end
 
   describe "#find_the_cheese" do
-    it "finds takes in an argument of an array and returns the first item in that array that is a cheese" do
-      cheddar_cheese = %w(banana cheddar sock)
+    it "returns the first element of the array that is cheese" do
+      cheddar_cheese = ["banana", "cheddar", "sock"]
       expect(find_the_cheese(cheddar_cheese)).to eq 'cheddar'
+    end
 
-      no_cheese = %w(ham cellphone computer)
+    it "returns nill if the array does not contain a type of cheese" do
+      no_cheese = ["ham", "cellphone", "computer"]
       expect(find_the_cheese(no_cheese)).to eq nil
-
-      camembert_cheese = %w(owl blanket camembert)
-      expect(find_the_cheese(camembert_cheese)).to eq 'camembert'
-
-      gouda_cheese = %w(gouda cheddar camembert pontoons)
-      expect(find_the_cheese(gouda_cheese)).to eq 'gouda'
     end
   end
 
