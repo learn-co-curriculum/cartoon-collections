1f8a00edb51377c96da90083d6eeee2667469f4d
AnnJohn <ann@flatironschool.com>
2015-11-23 14:23:39 -0500

    Fixed typo on line 9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet# code an argument here
   # Your code here
 end
 
-def long_planteer_calls# code an argument here
+def long_planeteer_calls# code an argument here
   # Your code here
 end
 
