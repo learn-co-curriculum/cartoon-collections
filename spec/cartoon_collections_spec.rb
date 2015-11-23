c29d61a084f053875ca8a5fc36d3a527b5aad82b
Daniel Fenjves <dfenjves@users.noreply.github.com>
2015-11-23 09:31:35 -0500

    Update cartoon_collections_spec.rb

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -40,13 +40,13 @@ describe "Cartoon Collections" do
 
   describe "#long_planteer_calls" do
     it "returns true if any calls are longer than 4 characters" do
-      long_planteer_calls = ["earth", "wind", "fire", "water", "heart"]
-      expect(long_planteer_calls(long_planeteer_calls)).to eq(true)
+      long_calls = ["earth", "wind", "fire", "water", "heart"]
+      expect(long_planeteer_calls(long_calls)).to eq(true)
     end
 
     it "returns false if all calls are shorter than 4 characters" do
-      short_planeteer_calls = ["wind", "fire"]
-      expect(long_planteer_calls(short_planeteer_calls)).to eq(false)
+      short_calls = ["wind", "fire"]
+      expect(long_planeteer_calls(short_calls)).to eq(false)
     end
 
   end
