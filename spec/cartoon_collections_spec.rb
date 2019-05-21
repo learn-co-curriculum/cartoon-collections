4f5d73ae04240371d11acfa3948facd7c7337afe
Maxwell Benton <maxwellbenton@gmail.com>
2019-05-21 16:02:09 -0400

    fixes test message typo

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -44,7 +44,7 @@ describe "Cartoon Collections" do
       expect(long_planeteer_calls(calls_long)).to eq(true)
     end
 
-    it "returns false if all calls are shorter than 4 characters" do
+    it "returns false if all calls are 4 characters or less" do
       calls_short = ["wind", "fire", "tree", "axe", "code"]
       expect(long_planeteer_calls(calls_short)).to eq(false)
     end
