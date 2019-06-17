6758a9f03ce8824baf5bdb198faaa8bf9d4cefb9
Kellye Greene <kellye.greene@flatironschool.com>
2019-06-17 10:56:28 -0400

    Removes merge conflict flags

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -1,9 +1,9 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe "Cartoon Collections" do
-  describe "#roll_call_dwarves" do
-    it "prints out the 7 dwarfs in a numbered list" do
-      dwarves = ["Dopey", "Grumpy", "Bashful"]
+describe 'Cartoon Collections' do
+  describe '#roll_call_dwarves' do
+    it 'prints out the 7 dwarfs in a numbered list' do
+      dwarves = %w[Dopey Grumpy Bashful]
 
       output = capture_stdout do
         roll_call_dwarves(dwarves)
@@ -17,24 +17,24 @@ describe "Cartoon Collections" do
     end
   end
 
-  describe "#summon_captain_planet" do
-    it "returns an array with the same number of elements that it was given" do
-      veggies = ["carrot", "cucumber", "pepper"]
+  describe '#summon_captain_planet' do
+    it 'returns an array with the same number of elements that it was given' do
+      veggies = %w[carrot cucumber pepper]
       result = summon_captain_planet(veggies)
       expect(result.class).to eq(Array)
       expect(result.length).to eq(3)
     end
 
-    it "capitalizes each element and adds an exclamation mark (test 1)" do
-      fruits = ["apple", "banana", "orange"]
+    it 'capitalizes each element and adds an exclamation mark (test 1)' do
+      fruits = %w[apple banana orange]
       result = summon_captain_planet(fruits)
       ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
     end
 
-    it "capitalizes each element and adds an exclamation mark (test 2)" do
-      veggies = ["carrot", "cucumber", "pepper"]
+    it 'capitalizes each element and adds an exclamation mark (test 2)' do
+      veggies = %w[carrot cucumber pepper]
       result = summon_captain_planet(veggies)
-      ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
+      ['Carrot!', 'Cucumber!', 'Pepper!'].each { |w| expect(result).to include w }
     end
   end
 
@@ -45,28 +45,19 @@ describe "Cartoon Collections" do
     end
 
     it "returns false if all calls are 4 characters or less" do
-<<<<<<< HEAD
-      calls_short = ["wind", "fire", "tree", "axe", "code"]
-      expect(long_planeteer_calls(calls_short)).to eq(false)
-=======
     calls_short = ["wind", "fire", "tree", "axe", "code"]
     expect(long_planeteer_calls(calls_short)).to eq(false)
->>>>>>> 1367bb9... Updates spec description
     end
-
   end
 
-  describe "#find_the_cheese" do
-    it "returns the first element of the array that is cheese" do
-      contains_cheddar = ["banana", "cheddar", "sock"]
-      expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
-
-      contains_gouda = ["potato", "gouda", "camembert"]
-      expect(find_the_cheese(contains_gouda)).to eq 'gouda'
+  describe '#find_the_cheese' do
+    it 'returns the first element of the array that is cheese' do
+      cheddar_cheese = %w[banana cheddar sock]
+      expect(find_the_cheese(cheddar_cheese)).to eq 'cheddar'
     end
 
-    it "returns nil if the array does not contain a type of cheese" do
-      no_cheese = ["ham", "cellphone", "computer"]
+    it 'returns nil if the array does not contain a type of cheese' do
+      no_cheese = %w[ham cellphone computer]
       expect(find_the_cheese(no_cheese)).to eq nil
     end
   end
